5a6a559b4fdf6b86352480816ac0ab974f9043ad
PeterBourgonje <bourgonje@gmail.com>
2024-02-19 14:24:14 +0100

    script for disrpt2golf format conversion

---
+++ rels2pipe_separated.py
@@ -0,0 +1,72 @@
+import os
+import sys
+import csv
+
+sec = [
+        'Temporal.Asynchronous',
+        'Temporal.Synchrony',
+        'Contingency.Cause',
+        'Contingency.Pragmatic cause',
+        'Comparison.Contrast',
+        'Comparison.Concession',
+        'Expansion.Conjunction',
+        'Expansion.Instantiation',
+        'Expansion.Restatement',
+        'Expansion.Alternative',
+        'Expansion.List'
+        ]
+top = [
+        'Temporal',
+        'Contingency',
+        'Comparison',
+        'Expansion'
+        ]
+
+def process(folder, outfolder):
+
+    for f in os.listdir(folder):
+        af = os.path.join(folder, f)
+        if af.endswith('_train.rels'):
+            outlines = convert(af)
+            train_out = open(os.path.join(outfolder, 'train.txt'), 'w', encoding='utf8')
+            for line in outlines:
+                train_out.write(line + '\n')
+                
+        elif af.endswith('_dev.rels'):
+            outlines = convert(af)
+            dev_out = open(os.path.join(outfolder, 'dev.txt'), 'w', encoding='utf8')
+            for line in outlines:
+                dev_out.write(line + '\n')
+         
+        elif af.endswith('_test.rels'):
+            outlines = convert(af)
+            test_out = open(os.path.join(outfolder, 'test.txt'), 'w', encoding='utf8')
+            for line in outlines:
+                test_out.write(line + '\n')
+           
+def convert(relfile):
+            
+    rows = csv.DictReader(open(relfile), delimiter='\t')
+    outlines = []
+    for row in rows:
+        sense = row['label']
+        toplevel = sense.split('.')[0]
+        seclevel = '.'.join(sense.split('.')[:2])
+        sense_list = [toplevel, seclevel, 'and']
+        arg1 = row['unit1_txt']
+        arg2 = row['unit2_txt']
+        outline = ' ||| '.join([str(sense_list), str([None, None, None]), arg1, arg2])
+        if toplevel in top and seclevel in sec:
+            outlines.append(outline)
+    
+    return outlines
+        
+
+def main():
+    folder = sys.argv[1] # folder with disrpt data, e.g. https://github.com/disrpt/sharedtask2023/tree/main/data/ita.pdtb.luna
+    outfolder = sys.argv[2]
+    process(folder, outfolder)
+    
+
+if __name__ == '__main__':
+    main()
